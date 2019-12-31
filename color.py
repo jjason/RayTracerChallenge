82508b9f687387b3f7c72125e3ff72398a561561
Jamie Jason <jamie.jason@gmail.com>
2019-12-30 20:23:26 -0800

    Chapter 2 - Canvas
    Add a python class to represent a canvas, which is a grid of pixels
    represented by color objects.
    
    Provide support for:
    * Creating a canvas of specified size
    * Getting and setting a pixel's color
    * Converting a canvas to PPM format
    
    Fix bug in color where was doing something really stupid and
    transposing blue and green.
    
    Add tests for new functionality.

--- color.py
+++ color.py
@@ -18,30 +18,44 @@ class Color(Tuple):
         self.x = value
 
     @property
-    def blue(self):
-        return self.y
-
-    @blue.setter
-    def blue(self, value):
-        self.y = value
-
-    @property
     def green(self):
-        return self.z
+        return self.y
 
     @green.setter
     def green(self, value):
+        self.y = value
+
+    @property
+    def blue(self):
+        return self.z
+
+    @blue.setter
+    def blue(self, value):
         self.z = value
+
+    @staticmethod
+    def _clamp_color_value(value):
+        # We want to take the color values that are supposed to be a float
+        # [0.0, 1.0] and convert to an integer value [0, 255].  Take the float
+        # value, multiply by 255, add 0.5 (to round properly), and convert to
+        # integer.  Then convert values below 0 to 0 and values above 255 to
+        # 255.
+        return min(max(0, int(value * 255 + 0.5)), 255)
 
     def __mul__(self, rhs):
         # If we are multiplying by another color, then we are going to compute
         # the Hadamard (or Schur) product, which just multiplies each of the
         # RGB components
         if isinstance(rhs, Color):
-            return Color(self.red * rhs.red,
-                         self.blue * rhs.blue,
-                         self.green * rhs.green)
+            return Color(red=(self.red * rhs.red),
+                         green=(self.green * rhs.green),
+                         blue=(self.blue * rhs.blue))
 
         # If we are not multiplying by another color, then fall back to the
         # scalar multiplication provided by the base class
         return super().__mul__(rhs)
+
+    def __str__(self):
+        return "{} {} {}".format(self._clamp_color_value(self.red),
+                                 self._clamp_color_value(self.green),
+                                 self._clamp_color_value(self.blue))
